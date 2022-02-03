2b0c7a4a7ffe5b8d01a12a34e7b40adc4719ec38
YOU310 <75014820+YOU310@users.noreply.github.com>
2022-02-03 12:00:00 +0900

    データ取り込みコードの修正

--- lib/import_csv.rb
+++ lib/import_csv.rb
@@ -4,11 +4,7 @@ class ImportCsv
   # CSVデータのパスを引数として受け取り、インポート処理を実行
   def self.import(path)
      CSV.foreach(path, headers: true) do |row|
-       User.create!(
-         name: row["name"],
-         age: row["age"],
-         address: row["address"]
-       )
+      User.create!(row.to_h)
      end
   end
 end
